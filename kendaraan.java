72643d0dad2d1fdf66e086de7fd59afeded1acf0
Sendi Purnomo <sendi.purnomo127@smk.belajar.id>
2026-04-14 11:28:00 +0700

    Add files via upload

--- kendaraan.java
+++ kendaraan.java
@@ -1,1 +1,30 @@
+package kendaraan;
+class Kendaraan {
+    void jalan() {
+        System.out.println("Kendaraan sedang berjalan");
+    }
+}
+class Mobil extends Kendaraan {
+    @Override
+    void jalan() {
+        System.out.println("Mobil berjalan di jalan raya");
+    }
+}
+class Sepeda extends Kendaraan {
+    @Override
+    void jalan() {
+        System.out.println("Sepeda dikayuh di jalan kecil");
+    }
+}
+public class kendaraan {
+    public static void main(String[] args) {
+        Kendaraan k = new Kendaraan();
+        k.jalan();
 
+        Mobil m = new Mobil();
+        m.jalan();
+
+        Sepeda s = new Sepeda();
+        s.jalan();
+    }
+}
